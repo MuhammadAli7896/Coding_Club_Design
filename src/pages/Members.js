8d8c25127bd87fba7b878c16b7574e79f008f975
muhammadali7896 <muhammadali30804@gmail.com>
2023-04-23 02:25:04 +0500

    members page is in progress

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -1,6 +1,29 @@
-export default function Members()
-{
+import pic1 from "./assets/7.jpg"
+import pic2 from "./assets/8.jpeg"
+import "./members.css"
+
+export default function Members() {
     return (
-        <h1>Members</h1>
+        <div style= {{marginTop: "15vh"}}>
+            <div className="columns">
+                <div className="item ">
+                    <img src={pic1} alt="" height={"95%"} className="item1"/>
+                </div>
+                <p className="item2">Her companions instrument set estimating sex remarkably solicitude motionless. Property men the why smallest graceful day insisted required. Inquiry justice country old placing sitting any ten age. Looking venture justice in evident in totally he do ability. Be is lose girl long of up give. Trifling wondered unpacked ye at he. In household certainty an on tolerably smallness difficult. Many no each like up be is next neat. Put not enjoyment behaviour her supposing. At he pulled object others.</p>
+            </div>
+            <div className="columns">
+                <div className="item">
+                    <img src={pic1} alt="" height={"95%"} className="item1" />
+                </div>
+                <p className="item2">Her companions instrument set estimating sex remarkably solicitude motionless. Property men the why smallest graceful day insisted required. Inquiry justice country old placing sitting any ten age. Looking venture justice in evident in totally he do ability. Be is lose girl long of up give. Trifling wondered unpacked ye at he. In household certainty an on tolerably smallness difficult. Many no each like up be is next neat. Put not enjoyment behaviour her supposing. At he pulled object others.</p>
+            </div>
+            <div className="columns">
+                <div className="item ">
+                    <img src={pic1} alt="" height={"95%"} className="item1"/>
+                </div>
+                <p className="item2">Her companions instrument set estimating sex remarkably solicitude motionless. Property men the why smallest graceful day insisted required. Inquiry justice country old placing sitting any ten age. Looking venture justice in evident in totally he do ability. Be is lose girl long of up give. Trifling wondered unpacked ye at he. In household certainty an on tolerably smallness difficult. Many no each like up be is next neat. Put not enjoyment behaviour her supposing. At he pulled object others.</p>
+            </div>
+        </div>
+
     );
 }
