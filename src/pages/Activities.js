dd79fdc0e8dba6e280b7f9405d19460a79d5920a
muhammadali7896 <muhammadali30804@gmail.com>
2023-04-24 17:45:30 +0500

    activities page created and designed

--- src/pages/Activities.js
+++ src/pages/Activities.js
@@ -1,6 +1,44 @@
-export default function Activities()
-{
+import "./activities.css"
+export default function Activities() {
     return (
-        <h1>Activities</h1>
+        <div className="all">
+            <h1 className="title">Activities</h1>
+            <div className="content">
+                <h2 className="heading">Projects</h2>
+                <p className="mainpara">Still court no small think death so an wrote. Incommode necessary no it behaviour convinced distrusts an unfeeling he. Could death since do we hoped is in. Exquisite no my attention extensive. The determine conveying moonlight age. Avoid for see marry sorry child. Sitting so totally forbade hundred to.</p>
+            </div>
+            <div className="subcontent">
+                <h3 className="subHeading">Project 1:</h3>
+                <p className="para">Still court no small think death so an wrote. Incommode necessary no it behaviour convinced distrusts an unfeeling he. Could death since do we hoped is in. Exquisite no my attention extensive. The determine conveying moonlight age. Avoid for see marry sorry child. Sitting so totally forbade hundred to.</p>
+            </div>
+            <div className="subcontent">
+                <h3 className="subHeading">Project 2:</h3>
+                <p className="para">Still court no small think death so an wrote. Incommode necessary no it behaviour convinced distrusts an unfeeling he. Could death since do we hoped is in. Exquisite no my attention extensive. The determine conveying moonlight age. Avoid for see marry sorry child. Sitting so totally forbade hundred to.</p>
+            </div>
+            <div className="content">
+                <h2 className="heading">Workshops</h2>
+                <p className="mainpara">Effects present letters inquiry no an removed or friends. Desire behind latter me though in. Supposing shameless am he engrossed up additions. My possible peculiar together to. Desire so better am cannot he up before points. Remember mistaken opinions it pleasure of debating. Court front maids forty if aware their at. Chicken use are pressed removed.</p>
+            </div>
+            <div className="subcontent">
+                <h3 className="subHeading">Workshop 1:</h3>
+                <p className="para">Effects present letters inquiry no an removed or friends. Desire behind latter me though in. Supposing shameless am he engrossed up additions. My possible peculiar together to. Desire so better am cannot he up before points. Remember mistaken opinions it pleasure of debating. Court front maids forty if aware their at. Chicken use are pressed removed.</p>
+            </div>
+            <div className="subcontent">
+                <h3 className="subHeading">Workshop 2:</h3>
+                <p className="para">Effects present letters inquiry no an removed or friends. Desire behind latter me though in. Supposing shameless am he engrossed up additions. My possible peculiar together to. Desire so better am cannot he up before points. Remember mistaken opinions it pleasure of debating. Court front maids forty if aware their at. Chicken use are pressed removed.</p>
+            </div>
+            <div className="content">
+                <h2 className="heading">Coding Competitions</h2>
+                <p className="mainpara">Effects present letters inquiry no an removed or friends. Desire behind latter me though in. Supposing shameless am he engrossed up additions. My possible peculiar together to. Desire so better am cannot he up before points. Remember mistaken opinions it pleasure of debating. Court front maids forty if aware their at. Chicken use are pressed removed.</p>
+            </div>
+            <div className="subcontent">
+                <h3 className="subHeading">Competition 1:</h3>
+                <p className="para">Effects present letters inquiry no an removed or friends. Desire behind latter me though in. Supposing shameless am he engrossed up additions. My possible peculiar together to. Desire so better am cannot he up before points. Remember mistaken opinions it pleasure of debating. Court front maids forty if aware their at. Chicken use are pressed removed.</p>
+            </div>
+            <div className="subcontent">
+                <h3 className="subHeading">Competition 2:</h3>
+                <p className="para">Effects present letters inquiry no an removed or friends. Desire behind latter me though in. Supposing shameless am he engrossed up additions. My possible peculiar together to. Desire so better am cannot he up before points. Remember mistaken opinions it pleasure of debating. Court front maids forty if aware their at. Chicken use are pressed removed.</p>
+            </div>
+        </div>
     );
 }
