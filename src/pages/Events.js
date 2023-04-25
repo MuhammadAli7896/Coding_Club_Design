935bc64e286740d4ee4f599b2cf7306dce786aeb
muhammadali7896 <muhammadali30804@gmail.com>
2023-04-25 15:21:04 +0500

    Update Events.js

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,6 +1,7 @@
 import pic4 from './assets/4.jpeg';
 import pic5 from './assets/5.jpeg';
 import pic6 from './assets/6.jpeg';
+import "./events.css";
 
 export default function Events() {
     let styles = {
@@ -42,10 +43,18 @@ export default function Events() {
     return (
         <div>
             <h1 style={styles.heading}>Events</h1>
-            <div>
+            <div className="contents">
                 <h3 style={styles.subHeading}>CODATHON Fall 2022</h3>
                 <p style={styles.paragraph}>Still court no small think death so an wrote. Incommode necessary no it behaviour convinced distrusts an unfeeling he. Could death since do we hoped is in. Exquisite no my attention extensive. The determine conveying moonlight age. Avoid for see marry sorry child. Sitting so totally forbade hundred to.</p>
-                <div style={{ display: "flex", margin: "0px 100px" }}>
+                <div className='image-container' style={{ display: "flex", margin: "0px 100px" }}>
+                    <img className="img-fluid " src={pic4} alt="" style={styles.image} />
+                    <img className="img-fluid " src={pic5} alt="" style={styles.image} />
+                </div>
+            </div>
+            <div className="contents">
+                <h3 style={styles.subHeading}>CODATHON Fall 2022</h3>
+                <p style={styles.paragraph}>Still court no small think death so an wrote. Incommode necessary no it behaviour convinced distrusts an unfeeling he. Could death since do we hoped is in. Exquisite no my attention extensive. The determine conveying moonlight age. Avoid for see marry sorry child. Sitting so totally forbade hundred to.</p>
+                <div className='image-container' style={{ display: "flex", margin: "0px 100px" }}>
                     <img className="img-fluid " src={pic4} alt="" style={styles.image} />
                     <img className="img-fluid " src={pic5} alt="" style={styles.image} />
                 </div>
@@ -53,20 +62,12 @@ export default function Events() {
             <div>
                 <h3 style={styles.subHeading}>CODATHON Fall 2022</h3>
                 <p style={styles.paragraph}>Still court no small think death so an wrote. Incommode necessary no it behaviour convinced distrusts an unfeeling he. Could death since do we hoped is in. Exquisite no my attention extensive. The determine conveying moonlight age. Avoid for see marry sorry child. Sitting so totally forbade hundred to.</p>
-                <div style={{ display: "flex", margin: "0px 100px" }}>
+                <div className='image-container' style={{ display: "flex", margin: "0px 100px" }}>
                     <img className="img-fluid " src={pic4} alt="" style={styles.image} />
                     <img className="img-fluid " src={pic5} alt="" style={styles.image} />
                 </div>
             </div>
-            <div>
-                <h3 style={styles.subHeading}>CODATHON Fall 2022</h3>
-                <p style={styles.paragraph}>Still court no small think death so an wrote. Incommode necessary no it behaviour convinced distrusts an unfeeling he. Could death since do we hoped is in. Exquisite no my attention extensive. The determine conveying moonlight age. Avoid for see marry sorry child. Sitting so totally forbade hundred to.</p>
-                <div style={{ display: "flex", margin: "0px 100px" }}>
-                    <img className="img-fluid " src={pic4} alt="" style={styles.image} />
-                    <img className="img-fluid " src={pic5} alt="" style={styles.image} />
-                </div>
-            </div>
-            <div>
+            <div className="contents">
                 <h3 style={styles.subHeading} >Upcoming Events</h3>
                 <h4 style={styles.subsubHeading}>HACKATHON June 2023</h4>
                 <p style={styles.paragraph}>Effects present letters inquiry no an removed or friends. Desire behind latter me though in. Supposing shameless am he engrossed up additions. My possible peculiar together to. Desire so better am cannot he up before points. Remember mistaken opinions it pleasure of debating. Court front maids forty if aware their at. Chicken use are pressed removed.</p>
@@ -78,11 +79,11 @@ export default function Events() {
                         <li>Rewards: Prizes</li>
                     </ul>
                 </div>
-                <div style={{ display: "block", height: "5vh" }}>
+                <div className='button' style={{ display: "block", height: "5vh" }}>
                     <button className='btn btn-outline-secondary' style={styles.button} >Register for it</button>
                 </div>
             </div>
-            <div>
+            <div className="contents">
                 {/* <h3 style={styles.subHeading} >Upcoming Events</h3> */}
                 <h4 style={styles.subsubHeading}>HACKATHON June 2023</h4>
                 <p style={styles.paragraph}>Effects present letters inquiry no an removed or friends. Desire behind latter me though in. Supposing shameless am he engrossed up additions. My possible peculiar together to. Desire so better am cannot he up before points. Remember mistaken opinions it pleasure of debating. Court front maids forty if aware their at. Chicken use are pressed removed.</p>
@@ -94,7 +95,7 @@ export default function Events() {
                         <li>Rewards: Prizes</li>
                     </ul>
                 </div>
-                <div style={{ display: "block", height: "5vh" }}>
+                <div className='button' style={{ display: "block", height: "5vh" }}>
                     <button className='btn btn-outline-secondary' style={styles.button} >Register for it</button>
                 </div>
             </div>
