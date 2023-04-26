e03138350961154338a18295cd4ad05e9eef68d3
muhammadali7896 <muhammadali30804@gmail.com>
2023-04-27 02:05:00 +0500

    Update App.js
    Modals inserted for login sign up forms

--- src/App.js
+++ src/App.js
@@ -43,13 +43,115 @@ function App() {
 								<Link className="nav-link" to="/contact">Contact</Link>
 							</li>
 						</ul>
-						{/* <form className="d-flex" role="search">
-							<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-							<button className="btn btn-outline-success" type="submit">Search</button>
-						</form> */}
+						<form className="d-flex" role="search">
+							{/* <!-- Button trigger login modal --> */}
+							<button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#loginModal" style={{marginRight: "10px"}}>
+								Login
+							</button>
+							{/* Button trigger signup modal */}
+							<button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#signupModal" style={{marginRight: "10px"}}>
+								Sign up
+							</button>
+						</form>
 					</div>
 				</div>
 			</nav>
+
+
+
+			{/* <!-- Sign up Modal --> */}
+
+			<div className="modal fade" id="signupModal" tabindex="-1" aria-labelledby="signupModalLabel" aria-hidden="true">
+				<div className="modal-dialog modal-dialog-centered">
+					<div className="modal-content">
+						<div className="modal-header">
+							<h1 className="modal-title fs-5" id="signupModalLabel">Sign Up Form</h1>
+							<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+						</div>
+						<div className="modal-body">
+							<form>
+								<div className="mb-3">
+									<label for="signupInputName" className="form-label">Username</label>
+									<input type="text" className="form-control" id="signupInputName" placeholder='Enter Username' />
+									{/* <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div> */}
+								</div>
+								<div className="mb-3">
+									<label for="signupInputEmail1" className="form-label">Email address</label>
+									<input type="email" className="form-control" id="signupInputEmail1" aria-describedby="emailHelp" placeholder='Enter valid email address' />
+									<div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+								</div>
+								<div className="mb-3">
+									<label for="signupInputPassword1" className="form-label">Password</label>
+									<input type="password" className="form-control" id="signupInputPassword1" placeholder='Enter Password' />
+								</div>
+							</form>
+						</div>
+						<div className="modal-footer">
+							<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+							<button type="button" className="btn btn-primary">Sign Up</button>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			{/* Login Modal	 */}
+
+			<div className="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
+				<div className="modal-dialog modal-dialog-centered">
+					<div className="modal-content">
+						<div className="modal-header">
+							<h1 className="modal-title fs-5" id="loginModalLabel">Login Form</h1>
+							<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+						</div>
+						<div className="modal-body">
+							<form>
+								<div className="mb-3">
+									<label for="loginInputName" className="form-label">Username</label>
+									<input type="text" className="form-control" id="loginInputName" placeholder='Enter Username' />
+								</div>
+								<div className="mb-3">
+									<label for="loginInputPassword1" className="form-label">Password</label>
+									<input type="password" className="form-control" id="loginInputPassword1" placeholder='Enter Password' />
+								</div>
+							</form>
+						</div>
+						<div className="modal-footer">
+							<div style={{ marginRight: "auto" }} >
+								<button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#emailModal">
+									Forgot Password
+								</button>
+							</div>
+							<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+							<button type="button" className="btn btn-primary">Login</button>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			{/* forget password modal */}
+
+			<div class="modal fade" id="emailModal" tabindex="-1" aria-labelledby="emailModalLabel" aria-hidden="true">
+				<div class="modal-dialog modal-dialog-centered">
+					<div class="modal-content">
+						<div class="modal-header">
+							<h1 class="modal-title fs-5" id="emailModalLabel">Modal title</h1>
+							<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+						</div>
+						<div class="modal-body">
+							<div className="mb-3">
+								<label for="signupInputEmail1" className="form-label">Email address</label>
+								<input type="email" className="form-control" id="signupInputEmail1" aria-describedby="emailHelp" placeholder='Enter valid email address' />
+								<div id="emailHelp" className="form-text">Enter email address to send password</div>
+							</div>
+						</div>
+						<div class="modal-footer">
+							{/* <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button> */}
+							<button type="button" class="btn btn-primary">Send</button>
+						</div>
+					</div>
+				</div>
+			</div>
+
 
 			<Routes>
 				<Route path='/' element={<Homepage />} />
@@ -64,8 +166,8 @@ function App() {
 			{/* footer */}
 			<div>
 				<div className='footer d-block pd-5'>
-					<footer class="container" >
-						<p class="float-end"><a href="#">Back to top</a></p>
+					<footer className="container" >
+						<p className="float-end"><a href="#">Back to top</a></p>
 						<p>© 2017–2023 Coding Club, Inc. ASAM codes  · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
 					</footer>
 				</div>
