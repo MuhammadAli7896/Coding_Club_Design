e2bc29a3ef34a7d23af4740b6c157c4bdeb40fcd
muhammadali7896 <muhammadali30804@gmail.com>
2023-04-29 01:22:34 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ function App() {
 
 			{/* <!-- Sign up Modal --> */}
 
-			<div className="modal fade" id="signupModal" tabindex="-1" aria-labelledby="signupModalLabel" aria-hidden="true">
+			<div className="modal fade" id="signupModal" tabIndex="-1" aria-labelledby="signupModalLabel" aria-hidden="true">
 				<div className="modal-dialog modal-dialog-centered">
 					<div className="modal-content">
 						<div className="modal-header">
@@ -69,17 +69,17 @@ function App() {
 						<div className="modal-body">
 							<form>
 								<div className="mb-3">
-									<label for="signupInputName" className="form-label">Username</label>
+									<label htmlFor="signupInputName" className="form-label">Username</label>
 									<input type="text" className="form-control" id="signupInputName" placeholder='Enter Username' />
 									{/* <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div> */}
 								</div>
 								<div className="mb-3">
-									<label for="signupInputEmail1" className="form-label">Email address</label>
+									<label htmlFor="signupInputEmail1" className="form-label">Email address</label>
 									<input type="email" className="form-control" id="signupInputEmail1" aria-describedby="emailHelp" placeholder='Enter valid email address' />
 									<div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
 								</div>
 								<div className="mb-3">
-									<label for="signupInputPassword1" className="form-label">Password</label>
+									<label htmlFor="signupInputPassword1" className="form-label">Password</label>
 									<input type="password" className="form-control" id="signupInputPassword1" placeholder='Enter Password' />
 								</div>
 							</form>
@@ -94,7 +94,7 @@ function App() {
 
 			{/* Login Modal	 */}
 
-			<div className="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
+			<div className="modal fade" id="loginModal" tabIndex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
 				<div className="modal-dialog modal-dialog-centered">
 					<div className="modal-content">
 						<div className="modal-header">
@@ -104,11 +104,11 @@ function App() {
 						<div className="modal-body">
 							<form>
 								<div className="mb-3">
-									<label for="loginInputName" className="form-label">Username</label>
+									<label htmlFor="loginInputName" className="form-label">Username</label>
 									<input type="text" className="form-control" id="loginInputName" placeholder='Enter Username' />
 								</div>
 								<div className="mb-3">
-									<label for="loginInputPassword1" className="form-label">Password</label>
+									<label htmlFor="loginInputPassword1" className="form-label">Password</label>
 									<input type="password" className="form-control" id="loginInputPassword1" placeholder='Enter Password' />
 								</div>
 							</form>
@@ -128,23 +128,23 @@ function App() {
 
 			{/* forget password modal */}
 
-			<div class="modal fade" id="emailModal" tabindex="-1" aria-labelledby="emailModalLabel" aria-hidden="true">
-				<div class="modal-dialog modal-dialog-centered">
-					<div class="modal-content">
-						<div class="modal-header">
-							<h1 class="modal-title fs-5" id="emailModalLabel">Modal title</h1>
-							<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+			<div className="modal fade" id="emailModal" tabIndex="-1" aria-labelledby="emailModalLabel" aria-hidden="true">
+				<div className="modal-dialog modal-dialog-centered">
+					<div className="modal-content">
+						<div className="modal-header">
+							<h1 className="modal-title fs-5" id="emailModalLabel">Modal title</h1>
+							<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 						</div>
-						<div class="modal-body">
+						<div className="modal-body">
 							<div className="mb-3">
-								<label for="signupInputEmail1" className="form-label">Email address</label>
+								<label htmlFor="signupInputEmail1" className="form-label">Email address</label>
 								<input type="email" className="form-control" id="signupInputEmail1" aria-describedby="emailHelp" placeholder='Enter valid email address' />
 								<div id="emailHelp" className="form-text">Enter email address to send password</div>
 							</div>
 						</div>
-						<div class="modal-footer">
-							{/* <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button> */}
-							<button type="button" class="btn btn-primary">Send</button>
+						<div className="modal-footer">
+							{/* <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button> */}
+							<button type="button" className="btn btn-primary">Send</button>
 						</div>
 					</div>
 				</div>
