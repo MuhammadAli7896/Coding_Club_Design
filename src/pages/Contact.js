57300e1024f53f9ff5fc03391e9477a044383954
muhammadali7896 <muhammadali30804@gmail.com>
2023-04-29 01:35:01 +0500

    Update Contact.js
    submit button added

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,10 +3,10 @@ export default function Contact() {
         <div className="container my-3">
             <h1>Contact Us</h1>
             <div className="mb-3">
-                <label for="exampleFormControlInput1" className="form-label">Email address</label>
+                <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
                 <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
             </div>
-            <label for="" className="form-label">Select category </label>
+            <label htmlFor="" className="form-label">Select category </label>
             <select className="form-select" aria-label="Default select example">
                 <option selected>Competitions</option>
                 <option value="1">Courses</option>
@@ -17,34 +17,37 @@ export default function Contact() {
                 <option value="4">Learning</option>
                 <option value="5">Events</option>
             </select>
-            
-            <span style={{marginRight : "25px"}}>Are you a member? </span>
-            <div class="form-check my-3 d-inline-block">
-                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" />
-                    <label class="form-check-label" for="flexCheckChecked">
-                        Yes
-                    </label>
+
+            <span style={{ marginRight: "25px" }}>Are you a member? </span>
+            <div className="form-check my-3 d-inline-block">
+                <input className="form-check-input" type="checkbox" value="" id="flexCheckChecked" />
+                <label className="form-check-label" htmlFor="flexCheckChecked">
+                    Yes
+                </label>
             </div>
 
 
             <div className="mb-3 my-2">
-                <label for="exampleFormControlTextarea1" className="form-label">Your query</label>
+                <label htmlFor="exampleFormControlTextarea1" className="form-label">Your query</label>
                 <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"
                     placeholder="Message or Question"></textarea>
             </div>
 
-            <label for="" className="form-label d-block">How you find this site?</label>
+            <label htmlFor="" className="form-label d-block">How you find this site?</label>
             <div className="form-check form-check-inline ml-4">
                 <input className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1" />
-                <label className="form-check-label" for="inlineCheckbox1">Yes, helpful</label>
+                <label className="form-check-label" htmlFor="inlineCheckbox1">Yes, helpful</label>
             </div>
             <div className="form-check form-check-inline">
                 <input className="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2" />
-                <label className="form-check-label" for="inlineCheckbox2">Need Improvement</label>
+                <label className="form-check-label" htmlFor="inlineCheckbox2">Need Improvement</label>
             </div>
             <div className="form-check form-check-inline">
                 <input className="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3" />
-                <label className="form-check-label" for="inlineCheckbox3">There are very issues</label>
+                <label className="form-check-label" htmlFor="inlineCheckbox3">There are very issues</label>
+            </div>
+            <div style={{marginTop: "5vh", justifyContent: "center", display: "flex"}}>
+                <button type="button" className="btn btn-primary btn-lg">Submit</button>
             </div>
         </div>
 
